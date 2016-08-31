333e95dcbc811b0cba4a5d9689af80b70c53a915
Avinash Kumar <avinashtelcom@gmail.com>
2016-08-31 08:14:14 +0000

    Step 4

--- solution.js
+++ solution.js
@@ -1,18 +1,22 @@
 var mongo = require("mongodb").MongoClient;
-var url = "mongodb://"+process.env.IP+":27017/learnyoumongo";
+var url = "mongodb://"+process.env.IP+":27017/"+process.argv[2];
 
 mongo.connect(url, function(err, db) {
     if(err) {
         throw err;
     }
-    var docs = db.collection("docs");
+    var users = db.collection("users");
     
-    var JSONobj = JSON.parse(JSON.stringify({
-        firstName: process.argv[2],
-        lastName: process.argv[3]
-    }));
+    users.update({
+        username: 'tinatime'
+    }, {
+        $set: {
+            age: 40
+        }
+    }, function(err) {
+        if(err) throw err;
+        db.close();
+    });
+
     
-    docs.insert(JSONobj);
-    console.log(JSON.stringify(JSONobj));
-    db.close();
 });
